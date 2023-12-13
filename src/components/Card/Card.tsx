59e678958986913d4f7b7bf24f5d8f59715aecfe
MatheusRelvinhas <matheusrelvinhas@gmail.com>
2023-12-12 22:47:36 -0300

    css-styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -24,13 +24,31 @@ export default function Card(props: CardProps) {
         color: selectedTheme ? dataCss.colorFont.dark : dataCss.colorFont.light,
       }}
     >
-      <a href={props.url} target="_blank" rel="noopener noreferrer">
       <Image src={props.img} alt={props.title} width={390} height={260} />
       <div className="card-content">
         <p className="card-title">{props.title}</p>
         <p className="card-description">{props.description}</p>
+        <a href={props.url} target="_blank" rel="noopener noreferrer">
+          <button className="animated-button">
+            <svg
+              viewBox="0 0 24 24"
+              className="arr-2"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+            </svg>
+            <span className="text">Link</span>
+            <span className="circle"></span>
+            <svg
+              viewBox="0 0 24 24"
+              className="arr-1"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+            </svg>
+          </button>
+        </a>
       </div>
-      </a>
     </div>
   );
 }
